72ddd54b3d5944a1f45803d88de9be9e303b532a
sofiqak <130180826+sofiqak@users.noreply.github.com>
2023-04-07 22:55:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 917079889 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x)
+    sum_x = np.sum(x)
+    L = (1/n * sum_x)/70
+    return L # Ваш ответ
